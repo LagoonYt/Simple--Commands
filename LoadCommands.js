13a65dbde146ba68d59909894933bb77007135c2
LagoonYt <85791247+LagoonYt@users.noreply.github.com>
2023-07-14 21:29:11 +0100

    Add files via upload

--- LoadCommands.js
+++ LoadCommands.js
@@ -1,25 +1,69 @@
 const path = require('path');
 const fs = require('fs');
+const readline = require('readline');
 
-module.exports = (clientId) => {
+const createCommandsFolder = (folderPath) => {
+  fs.mkdirSync(folderPath);
+  console.log(`Created 'commands' folder at ${folderPath}`);
+};
+
+const createSubfolders = (folderPath, subfolders) => {
+  for (const subfolder of subfolders) {
+    const subfolderPath = path.join(folderPath, subfolder);
+    fs.mkdirSync(subfolderPath);
+    console.log(`Created '${subfolder}' subfolder at ${subfolderPath}`);
+  }
+};
+
+const promptConfirmation = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question('Commands folder does not exist. Do you want to create it? (yes/no): ', (answer) => {
+      rl.close();
+      resolve(answer === 'yes');
+    });
+  });
+};
+
+module.exports = async (clientId) => {
   const commands = [];
   const clearAllCommands = process.env.CLEAR_COMMANDS === 'yes';
 
   if (!clearAllCommands) {
-    const commandsDirectory = path.join(__dirname, '../commands');
-    const commandDirectories = fs.readdirSync(commandsDirectory);
+    const botRootDirectory = path.resolve(process.cwd());
+    const commandsDirectory = path.join(botRootDirectory, 'commands');
 
-    for (const directory of commandDirectories) {
-      console.log('\n\u001b[33m' + directory + ':\u001b[0m');
-      const commandFiles = fs.readdirSync(path.join(commandsDirectory, directory)).filter(file => file.endsWith('.js'));
+    if (!fs.existsSync(commandsDirectory)) {
+      console.error('Commands directory not found.');
 
-      for (const file of commandFiles) {
-        const command = require(`./${path.join('../commands', directory, file)}`);
-        commands.push(command.data.toJSON());
-        console.log('  \u001b[32m' + file + '\u001b[0m has been loaded!');
+      const shouldCreateFolder = await promptConfirmation();
+
+      if (shouldCreateFolder) {
+        createCommandsFolder(commandsDirectory);
+        createSubfolders(commandsDirectory, ['admin', 'entertainment']);
+        return commands;
+      } else {
+        return commands;
+      }
+    } else {
+      const commandDirectories = fs.readdirSync(commandsDirectory);
+
+      for (const directory of commandDirectories) {
+        console.log('\n\u001b[33m' + directory + ':\u001b[0m');
+        const commandFiles = fs.readdirSync(path.join(commandsDirectory, directory)).filter(file => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+          const command = require(path.join(commandsDirectory, directory, file));
+          commands.push(command);
+          console.log('  \u001b[32m' + file + '\u001b[0m has been loaded!');
+        }
       }
     }
   }
 
-  return commands; // Return the commands array
+  return commands;
 };
--- RegisterCommands.js
+++ RegisterCommands.js
@@ -1,40 +1,35 @@
 const registerCommands = async (client, commands, guildId) => {
-    console.log('Registering loaded commands...');
-  
-    // Check if commands is an array
-    if (!Array.isArray(commands)) {
-      console.error('Commands must be an array.');
-      return;
-    }
-  
-    const successfulCommands = [];
-    const failedCommands = [];
-  
-    // Register the bot's slash commands
-    for (const command of commands) {
-      try {
-        if (guildId) {
-          // Register command in a specific guild
-          await client.guilds.cache.get(guildId)?.commands.create(command);
-        } else {
-          // Register command globally
-          await client.application?.commands.create(command);
-        }
-        successfulCommands.push(command.name);
-      } catch (error) {
-        console.error(`Failed to register command '${command.name}':`, error);
-        failedCommands.push(command.name);
+  console.log('Registering loaded commands...');
+
+  if (!Array.isArray(commands)) {
+    console.error('Commands must be an array.');
+    return;
+  }
+
+  const successfulCommands = [];
+  const failedCommands = [];
+
+  for (const command of commands) {
+    try {
+      if (guildId) {
+        await client.guilds.cache.get(guildId)?.commands.create(command);
+      } else {
+        await client.application?.commands.create(command);
       }
+      successfulCommands.push(command.name);
+    } catch (error) {
+      console.error(`Failed to register command '${command.name}':`, error);
+      failedCommands.push(command.name);
     }
-  
-    if (successfulCommands.length > 0) {
-      console.log('\u001b[32m' + "Commands registered successfully!");
-    }
-  
-    if (failedCommands.length > 0) {
-      console.error(`Failed to register commands: ${failedCommands.join(', ')}`);
-    }
-  };
-  
-  module.exports = registerCommands;
-  
+  }
+
+  if (successfulCommands.length > 0) {
+    console.log('\u001b[32m' + 'Commands registered successfully!');
+  }
+
+  if (failedCommands.length > 0) {
+    console.error(`Failed to register commands: ${failedCommands.join(', ')}`);
+  }
+};
+
+module.exports = registerCommands;
