91fa8d02325ab5fb08e1d55fa5464df46107983c
LagoonYt <85791247+LagoonYt@users.noreply.github.com>
2023-07-15 15:24:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const ClearCommands = require('./ClearCommands');
 
 module.exports = {
   commands: {
-    Load: LoadCommands,
-    Register: RegisterCommands,
-    Clear: ClearCommands,
+    load: LoadCommands,
+    register: RegisterCommands,
+    clear: ClearCommands,
   },
 };
